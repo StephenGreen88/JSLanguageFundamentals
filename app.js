976eba176df1f14e7ebd992b1e77073ef40d7e55
Stephen Green <stephengreen1028@gmail.com>
2020-02-09 20:08:33 -0700

    Finished the DOM manipulation.. Tasks can be added, removed, all cleared, however the filtering option isn't working right now so it'll need to be debugged later.

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ const taskInput = document.querySelector('#task');
 // Load all events listeners
 loadEvenListeners();
 
-// Load all event listeners function
+// Load all event listener functions
 function loadEvenListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task Event
+    taskList.addEventListener('click', removeTask);
+    // Clear tasks event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -42,4 +48,39 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Remove Task <-- Remove a single task per click
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) { // <-- The confirm alerts out the 'Are you sure?' string to the user
+            e.target.parentElement.parentElement.remove(); // <-- Using parentElement.parentElement to move up the DOM and remove the entire list item not just the icon
+        }
+    }
+}
+
+// Clear Tasks <-- Clear all tasks by clicking the 'clear tasks' button
+function clearTasks() {
+    // Slower option
+    // taskList.innerHTML = ''; 
+
+    // Faster option using a while loop
+    // This loop basically says, "While there's a firstChild, remove it")
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase(); // <-- This will give us whatever is being typed in and convert it to lower case
+
+    document.querySelectorAll('.collection-item').forEach(function (task) { // <-- We can use forEach method because querySelectorAll returns a node list
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) { // <-- We're grabbing the index of whatever was typed in, it it's -1 that means there's nothing left to loop through
+            task.style.dispaly = 'block'; // <-- If something's returned 'block' will show it
+        } else {
+            task.style.dispaly = 'none'; // <-- If nothing's returned nothing will be displayed
+        }
+    });
 }
